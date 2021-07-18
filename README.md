d8f54887ad9fe71c072f5338d0c5df0c2dcbb933
Mehdi Behira <mehdibehira@gmail.com>
2021-07-18 20:59:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# my_custom_link
+ 
---
+++ app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { Storage } from "./src/data/Storage.js";
+import { cutURL } from "./src/cutURL.js";
+import { redirect } from "./src/redirect.js";
+
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+app.use(express.json({ extended: false }));
+app.use(express.static('public'));
+app.use('/images', express.static('/public'));
+
+
+if (Storage.data.links === undefined) {
+  Storage.data.links = [];
+  Storage.write();
+}
+
+app.post("/cut", cutURL);
+app.get("/[a-f0-9]{5}", redirect);
+
+app.get('/', function(req, res){
+  res.sendFile('/index.html', {root: "."});
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
